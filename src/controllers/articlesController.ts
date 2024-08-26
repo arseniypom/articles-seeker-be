a4e74ef6e59e6b833aaef01f98d7c36de56c1ce8
arseniypom <arpomazkov@bk.ru>
2024-08-26 12:28:41 +0300

    Fixed pagination

--- src/controllers/articlesController.ts
+++ src/controllers/articlesController.ts
@@ -13,11 +13,11 @@ export const getArticles = async (req: Request<{}, {}, {}, IArticlesRequestQuery
       return res.status(200).json({ topic, articles: [], totalPages: 1 });
     }
 
-    const pageNumber = parseInt(page as string, 10) || 1;
+    const pageNumber = parseInt(page, 10) || 1;
 
     const articles = await findArticlesByTopic(topic, pageNumber, model);
 
-    res.status(200).json({ topic, articles, totalPages: 10 });
+    res.status(200).json({ topic, articles, totalPages: articles.length ? 10 : 1 });
   } catch (error) {
     console.error('Error handling request:', error);
     res.status(500).json({ error: 'Internal Server Error' });
